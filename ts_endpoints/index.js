81e02c235f88856d36566dcc56febaebc008f905
Alberto Medina <a.medina@irisbond.com>
2021-04-24 10:53:18 +0200

    añadiendo /findAll

--- ts_endpoints/index.js
+++ ts_endpoints/index.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const app = express();
+
 const sqlite3 = require('sqlite3').verbose()
 
 let db = new sqlite3.Database('../sherpa.db', (err) => {
@@ -10,7 +13,25 @@ let db = new sqlite3.Database('../sherpa.db', (err) => {
 
 })
 
-let sql = `SELECT * FROM Localizacion`;
+app.get('/api/:cp/findAll', (req, res) => {
+  let result = [];
+  let sql = `SELECT user FROM Localizacion WHERE cp = ${req.params.cp}`
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      console.log(row);
+      result.append(row.user)
+    });
+    
+  });
+  return result
+})
+
+app.listen(5001, () => { console.log(`Listening on port 5001`) })
+
+/*let sql = `SELECT * FROM Localizacion`;
 
 db.all(sql, [], (err, rows) => {
   if (err) {
@@ -26,4 +47,4 @@ db.close((err) => {
         return console.error(err.message);
     }
     console.log('Close the database connection.');
-});
+}); */
