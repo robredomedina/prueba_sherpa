49805b39fa8c426ad132e36d4cc63fc323344bd1
Alberto Medina <a.medina@irisbond.com>
2021-04-24 10:53:46 +0200

    cambiado el modelo

--- models.py
+++ models.py
@@ -3,14 +3,13 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    location = db.relationship('Localizacion', backref='user', lazy=True)
 
     def __repr__(self):
         return("User: {}".format(self.username))
 
 class Localizacion(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    username = db.Column(db.String(20), db.ForeignKey('user.id'), nullable=False)
+    user = db.Column(db.String(20), db.ForeignKey('user.username'), nullable=False)
     cp = db.Column(db.String(5), unique=False, nullable=False)
     city = db.Column(db.String(20), unique=False, nullable=False)
 
